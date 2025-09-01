86f847f81a135db2c14011d10792fb8505d78a9c
David Hunt <david@foscomputerservices.com>
2025-09-01 12:31:39 -0500

    Added a few more overloads for Localizable support

--- Sources/FOSMVVM/SwiftUI Support/View.swift
+++ Sources/FOSMVVM/SwiftUI Support/View.swift
@@ -32,5 +32,26 @@ extension View {
     func withValidations(for fieldName: String) -> some View {
         FieldValidationsView(wrappedView: self, fieldId: .init(id: fieldName))
     }
+
+    /// Configures the view's title for purposes of navigation,
+    /// using a localized string.
+    ///
+    /// A view's navigation title is used to visually display
+    /// the current navigation state of an interface.
+    /// On iOS and watchOS, when a view is navigated to inside
+    /// of a navigation view, that view's title is displayed
+    /// in the navigation bar. On iPadOS, the primary destination's
+    /// navigation title is reflected as the window's title in the
+    /// App Switcher. Similarly on macOS, the primary destination's title
+    /// is used as the window title in the titlebar, Windows menu
+    /// and Mission Control.
+    ///
+    /// Refer to the <doc:Configure-Your-Apps-Navigation-Titles> article
+    /// for more information on navigation title modifiers.
+    ///
+    /// - Parameter titleKey: The key to a localized string to display.
+    func navigationTitle(_ localizable: some Localizable) -> some View {
+        navigationTitle(localizable.defaultedLocalizedString())
+    }
 }
 #endif
